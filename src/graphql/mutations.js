540c9f9c6b367142eb7648344db1defc4e0b32bc
orangefruit21 <borovskih.05021999@gmail.com>
2019-11-18 22:28:28 +0300

    Connect amplify and API

---
+++ src/graphql/mutations.js
@@ -0,0 +1,171 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createAnswer = `mutation CreateAnswer($input: CreateAnswerInput!) {
+  createAnswer(input: $input) {
+    id
+    gender
+    humanName
+    humanHead {
+      id
+      src
+      posX
+      posY
+    }
+    humanShirt {
+      id
+      src
+      posX
+      posY
+    }
+    humanJackets {
+      id
+      src
+      posX
+      posY
+    }
+    humanPants {
+      id
+      src
+      posX
+      posY
+    }
+    humanShoes {
+      id
+      src
+      posX
+      posY
+    }
+    humanAccessories {
+      id
+      src
+      posX
+      posY
+    }
+    hardSkillPoints
+    softSkillsPoints
+    hardSkillName
+    Initiative
+    Creativity
+    Adaptability
+    Reflection
+    Multitasking
+    ListeningSkills
+    Teamwork
+    CriticalThinking
+    TimeManagement
+  }
+}
+`;
+export const updateAnswer = `mutation UpdateAnswer($input: UpdateAnswerInput!) {
+  updateAnswer(input: $input) {
+    id
+    gender
+    humanName
+    humanHead {
+      id
+      src
+      posX
+      posY
+    }
+    humanShirt {
+      id
+      src
+      posX
+      posY
+    }
+    humanJackets {
+      id
+      src
+      posX
+      posY
+    }
+    humanPants {
+      id
+      src
+      posX
+      posY
+    }
+    humanShoes {
+      id
+      src
+      posX
+      posY
+    }
+    humanAccessories {
+      id
+      src
+      posX
+      posY
+    }
+    hardSkillPoints
+    softSkillsPoints
+    hardSkillName
+    Initiative
+    Creativity
+    Adaptability
+    Reflection
+    Multitasking
+    ListeningSkills
+    Teamwork
+    CriticalThinking
+    TimeManagement
+  }
+}
+`;
+export const deleteAnswer = `mutation DeleteAnswer($input: DeleteAnswerInput!) {
+  deleteAnswer(input: $input) {
+    id
+    gender
+    humanName
+    humanHead {
+      id
+      src
+      posX
+      posY
+    }
+    humanShirt {
+      id
+      src
+      posX
+      posY
+    }
+    humanJackets {
+      id
+      src
+      posX
+      posY
+    }
+    humanPants {
+      id
+      src
+      posX
+      posY
+    }
+    humanShoes {
+      id
+      src
+      posX
+      posY
+    }
+    humanAccessories {
+      id
+      src
+      posX
+      posY
+    }
+    hardSkillPoints
+    softSkillsPoints
+    hardSkillName
+    Initiative
+    Creativity
+    Adaptability
+    Reflection
+    Multitasking
+    ListeningSkills
+    Teamwork
+    CriticalThinking
+    TimeManagement
+  }
+}
+`;
