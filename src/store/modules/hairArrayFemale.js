cd7fd031f2c7875dc6b9589d21d9b9bb7dd9ae04
orangefruit21 <borovskih.05021999@gmail.com>
2019-11-24 22:52:15 +0300

    Complete layering

--- src/store/modules/hairArrayFemale.js
+++ src/store/modules/hairArrayFemale.js
@@ -7,70 +7,70 @@ export default {
                 src:require('../../assets/Textures/Hair/female/black/1.png'),
                 posX:368,
                 posY:129,
-                z:11
+                z:9
             },
             {
                 id:'2',
                 src:require('../../assets/Textures/Hair/female/black/2.png'),
                 posX:415,
                 posY:126,
-                z:11
+                z:9
             },
             {
                 id:'3',
                 src:require('../../assets/Textures/Hair/female/black/3.png'),
                 posX:390,
                 posY:123,
-                z:11
+                z:9
             },
             {
                 id:'4',
                 src:require('../../assets/Textures/Hair/female/black/4.png'),
                 posX:401,
                 posY:107,
-                z:11
+                z:9
             },
             {
                 id:'5',
                 src:require('../../assets/Textures/Hair/female/black/5.png'),
                 posX:419,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'6',
                 src:require('../../assets/Textures/Hair/female/black/6.png'),
                 posX:414,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'7',
                 src:require('../../assets/Textures/Hair/female/black/7.png'),
                 posX:404,
                 posY:110,
-                z:11
+                z:9
             },
             {
                 id:'8',
                 src:require('../../assets/Textures/Hair/female/black/8.png'),
                 posX:415,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'9',
                 src:require('../../assets/Textures/Hair/female/black/9.png'),
                 posX:429,
                 posY:109,
-                z:11
+                z:9
             },
             {
                 id:'10',
                 src:require('../../assets/Textures/Hair/female/black/10.png'),
                 posX:407,
                 posY:125,
-                z:11
+                z:9
             }
         ],
         // Женские  бордовые волосы
@@ -80,70 +80,70 @@ export default {
                 src:require('../../assets/Textures/Hair/female/bordo/1.png'),
                 posX:368,
                 posY:129,
-                z:11
+                z:9
             },
             {
                 id:'2',
                 src:require('../../assets/Textures/Hair/female/bordo/2.png'),
                 posX:415,
                 posY:126,
-                z:11
+                z:9
             },
             {
                 id:'3',
                 src:require('../../assets/Textures/Hair/female/bordo/3.png'),
                 posX:390,
                 posY:123,
-                z:11
+                z:9
             },
             {
                 id:'4',
                 src:require('../../assets/Textures/Hair/female/bordo/4.png'),
                 posX:401,
                 posY:107,
-                z:11
+                z:9
             },
             {
                 id:'5',
                 src:require('../../assets/Textures/Hair/female/bordo/5.png'),
                 posX:419,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'6',
                 src:require('../../assets/Textures/Hair/female/bordo/6.png'),
                 posX:414,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'7',
                 src:require('../../assets/Textures/Hair/female/bordo/7.png'),
                 posX:404,
                 posY:110,
-                z:11
+                z:9
             },
             {
                 id:'8',
                 src:require('../../assets/Textures/Hair/female/bordo/8.png'),
                 posX:415,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'9',
                 src:require('../../assets/Textures/Hair/female/bordo/9.png'),
                 posX:429,
                 posY:109,
-                z:11
+                z:9
             },
             {
                 id:'10',
                 src:require('../../assets/Textures/Hair/female/bordo/10.png'),
                 posX:407,
                 posY:125,
-                z:11
+                z:9
             }
         ],
         // Женские коричнивые волосы
@@ -153,70 +153,70 @@ export default {
                 src:require('../../assets/Textures/Hair/female/brown/1.png'),
                 posX:368,
                 posY:129,
-                z:11
+                z:9
             },
             {
                 id:'2',
                 src:require('../../assets/Textures/Hair/female/brown/2.png'),
                 posX:415,
                 posY:126,
-                z:11
+                z:9
             },
             {
                 id:'3',
                 src:require('../../assets/Textures/Hair/female/brown/3.png'),
                 posX:390,
                 posY:123,
-                z:11
+                z:9
             },
             {
                 id:'4',
                 src:require('../../assets/Textures/Hair/female/brown/4.png'),
                 posX:401,
                 posY:107,
-                z:11
+                z:9
             },
             {
                 id:'5',
                 src:require('../../assets/Textures/Hair/female/brown/5.png'),
                 posX:419,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'6',
                 src:require('../../assets/Textures/Hair/female/brown/6.png'),
                 posX:414,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'7',
                 src:require('../../assets/Textures/Hair/female/brown/7.png'),
                 posX:404,
                 posY:110,
-                z:11
+                z:9
             },
             {
                 id:'8',
                 src:require('../../assets/Textures/Hair/female/brown/8.png'),
                 posX:415,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'9',
                 src:require('../../assets/Textures/Hair/female/brown/9.png'),
                 posX:429,
                 posY:109,
-                z:11
+                z:9
             },
             {
                 id:'10',
                 src:require('../../assets/Textures/Hair/female/brown/10.png'),
                 posX:407,
                 posY:125,
-                z:11
+                z:9
             }
         ],
         // Женские рыжие волосы
@@ -226,70 +226,70 @@ export default {
                 src:require('../../assets/Textures/Hair/female/ginger/1.png'),
                 posX:368,
                 posY:129,
-                z:11
+                z:9
             },
             {
                 id:'2',
                 src:require('../../assets/Textures/Hair/female/ginger/2.png'),
                 posX:415,
                 posY:126,
-                z:11
+                z:9
             },
             {
                 id:'3',
                 src:require('../../assets/Textures/Hair/female/ginger/3.png'),
                 posX:390,
                 posY:123,
-                z:11
+                z:9
             },
             {
                 id:'4',
                 src:require('../../assets/Textures/Hair/female/ginger/4.png'),
                 posX:401,
                 posY:107,
-                z:11
+                z:9
             },
             {
                 id:'5',
                 src:require('../../assets/Textures/Hair/female/ginger/5.png'),
                 posX:419,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'6',
                 src:require('../../assets/Textures/Hair/female/ginger/6.png'),
                 posX:414,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'7',
                 src:require('../../assets/Textures/Hair/female/ginger/7.png'),
                 posX:404,
                 posY:110,
-                z:11
+                z:9
             },
             {
                 id:'8',
                 src:require('../../assets/Textures/Hair/female/ginger/8.png'),
                 posX:415,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'9',
                 src:require('../../assets/Textures/Hair/female/ginger/9.png'),
                 posX:429,
                 posY:109,
-                z:11
+                z:9
             },
             {
                 id:'10',
                 src:require('../../assets/Textures/Hair/female/ginger/10.png'),
                 posX:407,
                 posY:125,
-                z:11
+                z:9
             }
         ],
         // Женские зеленые волосы
@@ -299,70 +299,70 @@ export default {
                 src:require('../../assets/Textures/Hair/female/green/1.png'),
                 posX:368,
                 posY:129,
-                z:11
+                z:9
             },
             {
                 id:'2',
                 src:require('../../assets/Textures/Hair/female/green/2.png'),
                 posX:415,
                 posY:126,
-                z:11
+                z:9
             },
             {
                 id:'3',
                 src:require('../../assets/Textures/Hair/female/green/3.png'),
                 posX:390,
                 posY:123,
-                z:11
+                z:9
             },
             {
                 id:'4',
                 src:require('../../assets/Textures/Hair/female/green/4.png'),
                 posX:401,
                 posY:107,
-                z:11
+                z:9
             },
             {
                 id:'5',
                 src:require('../../assets/Textures/Hair/female/green/5.png'),
                 posX:419,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'6',
                 src:require('../../assets/Textures/Hair/female/green/6.png'),
                 posX:414,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'7',
                 src:require('../../assets/Textures/Hair/female/green/7.png'),
                 posX:404,
                 posY:110,
-                z:11
+                z:9
             },
             {
                 id:'8',
                 src:require('../../assets/Textures/Hair/female/green/8.png'),
                 posX:415,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'9',
                 src:require('../../assets/Textures/Hair/female/green/9.png'),
                 posX:429,
                 posY:109,
-                z:11
+                z:9
             },
             {
                 id:'10',
                 src:require('../../assets/Textures/Hair/female/green/10.png'),
                 posX:407,
                 posY:125,
-                z:11
+                z:9
             }
         ],
         // Женские светло-коричнивые волосы
@@ -372,70 +372,70 @@ export default {
                 src:require('../../assets/Textures/Hair/female/lightBrown/1.png'),
                 posX:368,
                 posY:129,
-                z:11
+                z:9
             },
             {
                 id:'2',
                 src:require('../../assets/Textures/Hair/female/lightBrown/2.png'),
                 posX:415,
                 posY:126,
-                z:11
+                z:9
             },
             {
                 id:'3',
                 src:require('../../assets/Textures/Hair/female/lightBrown/3.png'),
                 posX:390,
                 posY:123,
-                z:11
+                z:9
             },
             {
                 id:'4',
                 src:require('../../assets/Textures/Hair/female/lightBrown/4.png'),
                 posX:401,
                 posY:107,
-                z:11
+                z:9
             },
             {
                 id:'5',
                 src:require('../../assets/Textures/Hair/female/lightBrown/5.png'),
                 posX:419,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'6',
                 src:require('../../assets/Textures/Hair/female/lightBrown/6.png'),
                 posX:414,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'7',
                 src:require('../../assets/Textures/Hair/female/lightBrown/7.png'),
                 posX:404,
                 posY:110,
-                z:11
+                z:9
             },
             {
                 id:'8',
                 src:require('../../assets/Textures/Hair/female/lightBrown/8.png'),
                 posX:415,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'9',
                 src:require('../../assets/Textures/Hair/female/lightBrown/9.png'),
                 posX:429,
                 posY:109,
-                z:11
+                z:9
             },
             {
                 id:'10',
                 src:require('../../assets/Textures/Hair/female/lightBrown/10.png'),
                 posX:407,
                 posY:125,
-                z:11
+                z:9
             }
         ],
         // Женские красные волосы
@@ -445,70 +445,70 @@ export default {
                 src:require('../../assets/Textures/Hair/female/red/1.png'),
                 posX:368,
                 posY:129,
-                z:11
+                z:9
             },
             {
                 id:'2',
                 src:require('../../assets/Textures/Hair/female/red/2.png'),
                 posX:415,
                 posY:126,
-                z:11
+                z:9
             },
             {
                 id:'3',
                 src:require('../../assets/Textures/Hair/female/red/3.png'),
                 posX:390,
                 posY:123,
-                z:11
+                z:9
             },
             {
                 id:'4',
                 src:require('../../assets/Textures/Hair/female/red/4.png'),
                 posX:401,
                 posY:107,
-                z:11
+                z:9
             },
             {
                 id:'5',
                 src:require('../../assets/Textures/Hair/female/red/5.png'),
                 posX:419,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'6',
                 src:require('../../assets/Textures/Hair/female/red/6.png'),
                 posX:414,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'7',
                 src:require('../../assets/Textures/Hair/female/red/7.png'),
                 posX:404,
                 posY:110,
-                z:11
+                z:9
             },
             {
                 id:'8',
                 src:require('../../assets/Textures/Hair/female/red/8.png'),
                 posX:415,
                 posY:130,
-                z:11
+                z:9
             },
             {
                 id:'9',
                 src:require('../../assets/Textures/Hair/female/red/9.png'),
                 posX:429,
                 posY:109,
-                z:11
+                z:9
             },
             {
                 id:'10',
                 src:require('../../assets/Textures/Hair/female/red/10.png'),
                 posX:407,
                 posY:125,
-                z:11
+                z:9
             }
         ],
     },
--- src/store/modules/jacketsArray.js
+++ src/store/modules/jacketsArray.js
@@ -6,70 +6,70 @@ export default {
                 src:require('../../assets/Textures/Jackets/male/1.png'),
                 posX:365,
                 posY:332,
-                z:9
+                z:7
             },
             {
                 id:2,
                 src:require('../../assets/Textures/Jackets/male/2.png'),
                 posX:387,
                 posY:337,
-                z:9
+                z:7
             },
             {
                 id:3,
                 src:require('../../assets/Textures/Jackets/male/3.png'),
                 posX:361,
                 posY:337,
-                z:9
+                z:7
             },
             {
                 id:4,
                 src:require('../../assets/Textures/Jackets/male/4.png'),
                 posX:422,
                 posY:349,
-                z:9
+                z:7
             },
             {
                 id:5,
                 src:require('../../assets/Textures/Jackets/male/5.png'),
                 posX:353,
                 posY:340,
-                z:9
+                z:7
             },
             {
                 id:6,
                 src:require('../../assets/Textures/Jackets/male/6.png'),
                 posX:384,
                 posY:338,
-                z:9
+                z:7
             },
             {
                 id:7,
                 src:require('../../assets/Textures/Jackets/male/7.png'),
                 posX:292,
                 posY:342,
-                z:9
+                z:7
             },
             {
                 id:8,
                 src:require('../../assets/Textures/Jackets/male/8.png'),
                 posX:361,
                 posY:329,
-                z:9
+                z:7
             },
             {
                 id:9,
                 src:require('../../assets/Textures/Jackets/male/9.png'),
                 posX:372,
                 posY:331,
-                z:9
+                z:7
             },
             {
                 id:10,
                 src:require('../../assets/Textures/Jackets/male/10.png'),
                 posX:365,
                 posY:335,
-                z:9
+                z:7
             },
         ],
         jacketsFemale: [
@@ -78,105 +78,105 @@ export default {
                 src:require('../../assets/Textures/Jackets/female/1.png'),
                 posX:369,
                 posY:334,
-                z:9
+                z:7
             },
             {
                 id:2,
                 src:require('../../assets/Textures/Jackets/female/2.png'),
                 posX:365,
                 posY:321,
-                z:9
+                z:7
             },
             {
                 id:4,
                 src:require('../../assets/Textures/Jackets/female/4.png'),
                 posX:321,
                 posY:298,
-                z:9
+                z:7
             },
             {
                 id:5,
                 src:require('../../assets/Textures/Jackets/female/5.png'),
                 posX:387,
                 posY:331,
-                z:9
+                z:7
             },
             {
                 id:6,
                 src:require('../../assets/Textures/Jackets/female/6.png'),
                 posX:372,
                 posY:335,
-                z:9
+                z:7
             },
             {
                 id:7,
                 src:require('../../assets/Textures/Jackets/female/7.png'),
                 posX:372,
                 posY:323,
-                z:9
+                z:7
             },
             {
                 id:8,
                 src:require('../../assets/Textures/Jackets/female/8.png'),
                 posX: 422,
                 posY: 320,
-                z:9
+                z:7
             },
             {
                 id:9,
                 src:require('../../assets/Textures/Jackets/female/9.png'),
                 posX: 370,
                 posY: 335,
-                z:9
+                z:7
             },
             {
                 id:10,
                 src:require('../../assets/Textures/Jackets/female/10.png'),
                 posX: 377,
                 posY: 335,
-                z:9
+                z:7
             },
             {
                 id:11,
                 src:require('../../assets/Textures/Jackets/female/11.png'),
                 posX: 385,
                 posY: 340,
-                z:9
+                z:7
             },
             {
                 id:12,
                 src:require('../../assets/Textures/Jackets/female/12.png'),
                 posX: 373,
                 posY: 403,
-                z:9
+                z:7
             },
             {
                 id:13,
                 src:require('../../assets/Textures/Jackets/female/13.png'),
                 posX:360,
                 posY:310,
-                z:9
+                z:7
             },
             {
                 id:14,
                 src:require('../../assets/Textures/Jackets/female/14.png'),
                 posX:398,
                 posY:330,
-                z:9
+                z:7
             },
             {
                 id:15,
                 src:require('../../assets/Textures/Jackets/female/15.png'),
                 posX: 376,
                 posY: 338,
-                z:9
+                z:7
             },
             {
                 id:16,
                 src:require('../../assets/Textures/Jackets/female/16.png'),
                 posX: 371,
                 posY: 330,
-                z:9
+                z:7
             },
         ],
     }
